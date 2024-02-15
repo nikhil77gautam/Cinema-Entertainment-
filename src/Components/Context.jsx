cbea13c3fab2a48164d63b85803a29df4a095b13
Nikhil Gautam <135546239+nikhil77gautam@users.noreply.github.com>
2024-02-15 12:24:52 +0530

    Update Context.jsx

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 
 //My Movie API:
-export const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const AppContext = React.createContext();
 
